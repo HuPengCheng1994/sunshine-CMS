f438926c572145db8ecfa65a96f22876785146c6
HuPengCheng <Hi_HuPengCheng@163.com>
2019-05-14 10:36:20 +0800

    配置验证码

---
+++ config/captcha.php
@@ -0,0 +1,11 @@
+<?php
+$config =    [
+    // 验证码字体大小
+    'fontSize'    =>    30,    
+    // 验证码位数
+    'length'      =>    4,   
+    // 关闭验证码杂点
+    'useNoise'    =>    true, 
+];
+
+return $config;
